33ab27401fd35bfa2095cf550b64915fa726d9b6
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-03-11 03:22:23 -0300

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,16 +3,35 @@ import React from 'react';
 import styles from "../styles/contact.module.css"
 
 import Navbar from '../components/navbar';
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from '@mui/icons-material/Email';
 
 
 const Contact: React.FC = () => {
 
+    const linkedinUrl = "https://www.linkedin.com/in/mariajose-villarreal/"
+
     return (
-        <div className={styles.contactContainer}>
+        <div className={styles.contactSection}>
             <div className={styles.contact}>
                 <Navbar></Navbar>
                 <div className={styles.page}>
-                    <h1>This the contact page</h1>
+                    <h1>Here's where you can contact me!</h1>
+
+                    <div className={styles.contactContainer}>
+
+                        <div className={styles.contactLink}>
+                            <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon className={styles.contactIcon} /> María José Villarreal
+                            </a>
+                        </div>
+                    
+                        <div className={styles.contactLink}>
+                            <EmailIcon className={styles.contactIcon} /> villarrealmariaj@gmail.com
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </div>
