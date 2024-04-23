b0ffacb2e13254c7cfbf83d02344c141e6be174d
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-04-23 11:58:33 -0300

    adding pages

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,10 +12,7 @@ const About: React.FC = () => {
             <div className={styles.about}>
             <Navbar></Navbar>
                 <div className={styles.page}>
-                    <h1>Find out a little bit about me</h1>
-                    <p>I am a 22 years old frontend developer and Systems Engineering student from Mendoza</p>
-                    <h2>My certificates</h2>
-                    <h2>Hobbies</h2>
+                    <h1>Comming soon!</h1>
                 </div>
                 
             </div>
