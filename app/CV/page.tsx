2e234bba865dbdef67c1ab429d6e5695c3111a9a
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-05-14 19:14:54 -0300

    small style changes

--- app/CV/page.tsx
+++ app/CV/page.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
 
-import styles from "../styles/about.module.css"
+import styles from "../styles/cv.module.css"
 
 import Navbar from '../components/navbar';
 
+import Button from '@mui/material/Button';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const About: React.FC = () => {
 
     return (
-        <div className={styles.aboutContainer}>
-            <div className={styles.about}>
+        <div className={styles.cvContainer}>
+            <div className={styles.cv}>
             <Navbar></Navbar>
                 <div className={styles.page}>
                     <h1>View and download my resume</h1>
-                    <div>
-                        <iframe src="/archives/Resume-MariaJoseVillarreal.pdf" width="100%" height="800rem" />
-                    </div> 
+                        <div  className={styles.certificates}>
+
+                            <div className={styles.downloadContainer}>
+
+                                <img src='/images/CV_En.jpg'></img>
+                                
+                                <a href = '/archives/Resume-MariaJoseVillarreal.pdf' target = "_blank">
+                                    <Button
+                                        component="label"
+                                        variant="contained"
+                                        tabIndex={-1}
+                                        endIcon={<DownloadIcon />} >Download Resume
+                                    </Button>
+                                </a>
+
+                            </div>
+
+                            <div className={styles.downloadContainer}>
+
+                                <img src='/images/CV_Es.jpg'></img>
+
+                                <a href = '/archives/MariaJoseVillarreal-Resume.pdf' target = "_blank">
+                                    <Button
+                                        component="label"
+                                        variant="contained"
+                                        tabIndex={-1}
+                                        endIcon={<DownloadIcon />} >Descargar CV
+                                    </Button>
+                                </a>
+
+                            </div>
+
+                        </div>
                 </div>
                 
             </div>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,9 +15,18 @@ const About: React.FC = () => {
                     <h1>Find out a little bit about me</h1>
                         <p>I'm Majo, a 22 years old frontend developer and Systems Engineering student from Mendoza, Argentina.</p>
                     <h2>My certificates</h2>
-                    <div  className={styles.certificates}>
-                        <img src='/images/Fullstack_developer.jpg'></img>
-                        <img src='/images/CTD_Especializacion.jpg'></img>
+                    <div  className={styles.certificatesContainer}>
+                        <div className={styles.certificate}>
+                            <a href='/images/Fullstack_developer.jpg' target="_blank" rel="noopener noreferrer">
+                                <img src='/images/Fullstack_developer.jpg'></img>
+                            </a>
+                        </div>
+
+                        <div className={styles.certificate}>
+                            <a href='/images/CTD_Especializacion.jpg' target="_blank" rel="noopener noreferrer">
+                                <img src='/images/CTD_Especializacion.jpg'></img>
+                            </a>
+                        </div>
                     </div>
                     {/* <h2>Hobbies</h2> */}
                 </div>
