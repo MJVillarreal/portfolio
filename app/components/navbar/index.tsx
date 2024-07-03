665e7b96efba03a7b6fe5f8a201ffe964eaaae46
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-07-03 01:45:38 -0300

    responsive style to navbar

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,35 +1,50 @@
-import React from 'react';
+'use client';
+
+import React, { useState, ReactNode } from 'react';
 
 import styles from "../../styles/navbar.module.css"
 
 import Link from 'next/link';
 
+import MenuIcon from '@mui/icons-material/Menu';
+
+const NavbarItem = ({ href, children }: { href: string; children: ReactNode }) => {
+    return (
+      <li className={styles.navbarItem}>
+        <Link href={href}>
+          <p>{children}</p>
+        </Link>
+      </li>
+    );
+  };
+
 const Navbar = () => {
+
+    const [burgerMenuActive, setBurgerMenuActive] = useState(false);
+
+    const handleBurgerMenuClick = () => {
+      setBurgerMenuActive(!burgerMenuActive);
+    };
 
     return (
         <>
             <div className={styles.navbarContainer}>
                 <ul className={styles.navbarList}>
-                    <li className={styles.navbarItem}>
-                        <Link href="/about">
-                            <p>About Me</p>
-                        </Link>
-                    </li>
-                    <li className={styles.navbarItem}>
-                        <Link href="/projects">
-                            <p>Projects</p>
-                        </Link>
-                    </li>
-                    <li className={styles.navbarItem}>
-                        <Link href="/contact">
-                            <p>Contact</p>
-                        </Link>
-                    </li>
-                    <li className={styles.navbarItem}>
-                        <Link href="/CV">
-                            <p>Download CV</p>
-                        </Link>
-                    </li>
+                    <NavbarItem href="/about">About Me</NavbarItem>
+                    <NavbarItem href="/projects">Projects</NavbarItem>
+                    <NavbarItem href="/contact">Contact</NavbarItem>
+                    <NavbarItem href="/CV">Download CV</NavbarItem>
+                </ul>
+                <div className={`${styles.burgerMenu} ${burgerMenuActive? styles.active : ''}`} onClick={handleBurgerMenuClick}>
+                    <MenuIcon fontSize="large" />
+                </div>
+            </div>
+            <div className={`${styles.sidebar} ${burgerMenuActive? styles.active : ''}`}>
+                <ul className={styles.sidebarList}>
+                  <NavbarItem href="/about">About Me</NavbarItem>
+                  <NavbarItem href="/projects">Projects</NavbarItem>
+                  <NavbarItem href="/contact">Contact</NavbarItem>
+                  <NavbarItem href="/CV">Download CV</NavbarItem>
                 </ul>
             </div>
         </>
